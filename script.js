e2caeeb5ce0cb4f60c028282265887bf50e57ceb
Peter Marshall <peter.marshall@ocsbstudent.ca>
2021-02-18 17:59:02 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const numInput = document.getElementById('num')
+const playButton = document.getElementById('play')
+
+function onclick () {
+  if(numInput.value == '') {
+    alert('You must enter a number into the field.')
+  }
+  const num = parseInt(numInput.value)
+  if (num < 1 || num > 10) {
+    alert('Enter a number from 1 to 10.')
+  }
+  if (isNaN(num)) {
+    alert('You entered an invalid number')
+  }
+  const random = Math.ceil(Math.random() * 10)
+  if (random === num) {
+    win()
+  } else {
+    lose(random)
+  }
+  numInput.value = ''
+}
+
+function win () {
+  alert('You won!')
+}
+
+function lose (n) {
+  alert('I picked ' + n + '. Try again')
+}
+
+playButton.addEventListener('click', onclick)
